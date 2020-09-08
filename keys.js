999e76ac8e54d60c07b58f79f1316e5326e69232
“Harry <harry@prismoscope.com>
2020-09-08 14:56:36 -0400

    amazon s3 file upload

---
+++ keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  iam_access_id: 'AKIAI2MI3KBA7XLMY4BQ',
+  iam_secret: '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'
+}
--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express')
 const path = require('path')
 const socket = require('socket.io')
 const connectDB = require('./config/db')
+const multer = require('multer')
+const AWS = require('aws-sdk')
+const fs = require('fs')
+const keys = require('./keys.js')
 
 const app = express()
 
@@ -27,13 +31,6 @@ if (process.env.NODE_ENV === 'production') {
   })
 }
 
-//Server
-const PORT = process.env.PORT || 5000
-
-var server = app.listen(PORT, () =>
-  console.log(`Server started on port ${PORT}`)
-)
-
 //Socket.io
 const io = socket(server)
 
@@ -42,3 +39,87 @@ io.on('connection', (socket) => {
     io.emit('message', body)
   })
 })
+
+// configuring the DiscStorage engine.
+const storage = multer.diskStorage({
+  destination: 'uploads/',
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+const upload = multer({ storage: storage })
+
+//setting the credentials
+//The region should be the region of the bucket that you created
+//Visit this if you have any confusion - https://docs.aws.amazon.com/general/latest/gr/rande.html
+AWS.config.update({
+  accessKeyId: keys.iam_access_id,
+  secretAccessKey: keys.iam_secret,
+  region: 'us-east-2'
+})
+
+//Creating a new instance of S3:
+const s3 = new AWS.S3()
+
+//POST method route for uploading file
+app.post('/post_file', upload.single('demo_file'), function (req, res) {
+  //Multer middleware adds file(in case of single file ) or files(multiple files) object to the request object.
+  //req.file is the demo_file
+  uploadFile(req.file.path, req.file.filename, res)
+})
+
+//GET method route for downloading/retrieving file
+app.get('/get_file/:file_name', (req, res) => {
+  retrieveFile(req.params.file_name, res)
+})
+
+//The uploadFile function
+function uploadFile(source, targetName, res) {
+  console.log('preparing to upload...')
+  fs.readFile(source, function (err, filedata) {
+    if (!err) {
+      const putParams = {
+        Bucket: 'zespo',
+        Key: targetName,
+        Body: filedata
+      }
+      s3.putObject(putParams, function (err, data) {
+        if (err) {
+          console.log('Could nor upload the file. Error :', err)
+          return res.send({ success: false })
+        } else {
+          fs.unlink(source, () => {
+            console.log('local file deleted')
+          }) // Deleting the file from uploads folder(Optional).Do Whatever you prefer.
+          console.log('Successfully uploaded the file')
+          return res.send({ success: true })
+        }
+      })
+    } else {
+      console.log({ err: err })
+    }
+  })
+}
+
+//The retrieveFile function
+function retrieveFile(filename, res) {
+  const getParams = {
+    Bucket: 'zespo',
+    Key: filename
+  }
+
+  s3.getObject(getParams, function (err, data) {
+    if (err) {
+      return res.status(400).send({ success: false, err: err })
+    } else {
+      return res.send(data.Body)
+    }
+  })
+}
+
+//Server
+const PORT = process.env.PORT || 5000
+
+var server = app.listen(PORT, () =>
+  console.log(`Server started on port ${PORT}`)
+)
