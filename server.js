79b0f01fb1f0b073d229cabce14408baca4a845b
“Harry <harry@prismoscope.com>
2020-09-30 11:14:08 -0400

    redirect https

--- server.js
+++ server.js
@@ -34,6 +34,15 @@ if (process.env.NODE_ENV === 'production') {
   })
 }
 
+//force https
+app.use((req, res, next) => {
+  if (process.env.NODE_ENV === 'production') {
+    if (req.headers['x-forwarded-proto'] !== 'https')
+      return res.redirect('https://' + req.headers.host + req.url)
+    else return next()
+  } else return next()
+})
+
 // configuring the DiscStorage engine.
 const storage = multer.diskStorage({
   destination: 'uploads/',
